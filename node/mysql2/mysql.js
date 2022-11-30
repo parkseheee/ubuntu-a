d3d062e0cd2532ac0c9e961b4b906be0e045f8e4
bonugg <gboy77777777@gmail.com>
2022-11-30 15:33:59 +0900

    mysql

---
+++ node/mysql2/mysql.js
@@ -0,0 +1,35 @@
+var mysql = require('mysql2/promise')
+const env = require("dotenv").config({ path : "../.env" })
+
+const db = async () => {
+  try{
+    let connection = await
+    mysql.createConnection({
+      host : process.env.host,
+      user: process.env.user,
+      password : process.env.password,
+      database : process.env.database
+    })
+
+    let [rows, fields] = await connection.query("select * from st_info")
+    console.log(rows)
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+db();
+exit();
+
+function sleep(ms) {
+  return new Promise ((resolve) => {
+    setTimeout(resolve,ms)
+  })
+}
+
+async function exit() {
+  console.log(1);
+  await sleep(1000);
+  console.log(2);
+  process.exit(1);
+}
