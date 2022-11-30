d162d3c43a9fdb6b8e6468096ecd094429f39fea
bonugg <gboy77777777@gmail.com>
2022-11-30 13:56:08 +0900

    Create atlas.js

---
+++ atlas/atlas.js
@@ -0,0 +1,28 @@
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "things",
+    "database": "test",
+    "dataSource": "Cluster0",
+    "projection": {
+        "_id": 1
+    }
+});
+
+var config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-jdcso/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'jHxkmZWYgw3nhvgDwRvESfHMbCUfGFRPDBD6cqyUDiDqem3Nrj2TOgUtLtGeWTL0',
+    },
+    data: data
+};
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
